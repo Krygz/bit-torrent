bd4e58f9aa21a079abc61821ecf468b5fd0ac0cb
Pedro <ph4290245@gmail.com>
2024-11-17 16:23:37 -0300

    client creation

---
+++ client/client.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"net"
+	"time"
+
+	"github.com/Krygz/peers"
+)
+
+// A Client is a TCP connection with a peer
+type Client struct {
+	Conn     net.Conn
+	Choked   bool
+	Bitfield bitfield.BitField
+	peer     peers.Peer
+	infoHash [20]byte
+	peerID   [20]byte
+}
+
+func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
+	conn, err := net.DialTimeout("tcp", peer.String(), 3*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		Conn:     conn,
+		Choked:   true,
+		Bitfield: bf,
+		peer:     peer,
+		infoHash: infoHash,
+		peerID:   peerID,
+	}, nil
+}
