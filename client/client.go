96571a67753b26472d2d2e3ec632a9d4adb080f7
Pedro <ph4290245@gmail.com>
2024-11-17 23:19:55 -0300

    handshake creation

--- client/client.go
+++ client/client.go
@@ -17,6 +17,8 @@ type Client struct {
 	peerID   [20]byte
 }
 
+// New connects with a peer, completes a handshake, and receives a handshake
+// returns an err if any of those fail.
 func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
 	conn, err := net.DialTimeout("tcp", peer.String(), 3*time.Second)
 	if err != nil {
